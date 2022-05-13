2a3b3841a20d31b40889a2a2f3022ad16d619fc5
nashi <nn.contact.n@gmail.com>
2022-05-13 05:37:15 +0000

    [add] MainPageTemplateを実装

---
+++ components/templates/MainPageTemplate.tsx
@@ -0,0 +1,21 @@
+import { Box } from '@chakra-ui/react';
+import Head from 'next/head';
+import i from '../../text/js.json';
+
+export const MainPageTemplate = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <>
+      <Head>
+        <title>Portfolio</title>
+        <meta name="description" content={i.META_DESCRIPTION} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Box>{children}</Box>
+    </>
+  );
+};
