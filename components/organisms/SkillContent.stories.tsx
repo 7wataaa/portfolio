109376bd5f7b0f4f824f3095d731855c1a63dc3d
nashi <nn.contact.n@gmail.com>
2022-05-13 04:50:11 +0000

    [add] SkillContentを実装

---
+++ components/organisms/SkillContent.stories.tsx
@@ -0,0 +1,9 @@
+import { SkillContent } from './SkillContent';
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
+
+export default {
+  title: 'organisms/SkillContent',
+  component: SkillContent,
+} as ComponentMeta<typeof SkillContent>;
+
+export const Index: ComponentStoryObj<typeof SkillContent> = {};
