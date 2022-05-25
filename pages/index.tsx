f4e8e3d4d6c3c7c941afdbb2ec159b6c2a01f7d5
nashi <nn.contact.n@gmail.com>
2022-05-25 15:08:13 +0900

    [add] WorkContentをメインページに表示

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
 import type { NextPage } from 'next';
-import { AboutMeContent, Header, SkillContent } from '../components/organisms';
+import {
+  AboutMeContent,
+  Header,
+  SkillContent,
+  WorksContent,
+} from '../components/organisms';
 import { MainPageTemplate } from '../components/templates';
 
 const Home: NextPage = () => {
   return (
     <MainPageTemplate>
       <Header />
+      <WorksContent />
       <SkillContent />
       <AboutMeContent />
     </MainPageTemplate>
