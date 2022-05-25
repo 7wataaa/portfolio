3ca56ddd15d3149baea3fddf24c86cd01cb72a2b
nashi <nn.contact.n@gmail.com>
2022-05-25 13:32:56 +0900

    [add] MemoAppPageを実装

---
+++ pages/works/memoapp.tsx
@@ -0,0 +1,41 @@
+import { useBreakpointValue } from '@chakra-ui/react';
+import { Image } from '../../components/atoms';
+import { WorkCarousel, WorkDescription } from '../../components/organisms';
+import { WorkPageTemplate } from '../../components/templates';
+import i from '../../texts/js.json';
+
+const MemoAppPage = () => {
+  const imageSize = useBreakpointValue({
+    base: '500px',
+    sm: '300px',
+    md: '500px',
+  });
+
+  return (
+    <WorkPageTemplate>
+      <WorkCarousel
+        contents={[
+          {
+            main: (
+              <Image
+                width={imageSize}
+                height={imageSize}
+                alt="memoapp image"
+                src="/works/memoapp/memoapp.png"
+              />
+            ),
+          },
+        ]}
+      />
+      <WorkDescription data={i.WORKS.MEMOAPP} />
+    </WorkPageTemplate>
+  );
+};
+
+export default MemoAppPage;
+
+export async function getStaticProps() {
+  return {
+    props: {},
+  };
+}
