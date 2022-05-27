40fc7de8333fbea300492b2624714e10ef60721f
nashi <nn.contact.n@gmail.com>
2022-05-27 11:24:05 +0900

    [style] WorksContentにフェードイン効果を追加

--- components/organisms/WorksContent.tsx
+++ components/organisms/WorksContent.tsx
@@ -2,6 +2,7 @@ import { Box, Center, SimpleGrid } from '@chakra-ui/react';
 import i from '../../texts/js.json';
 import { Heading } from '../atoms';
 import { WorkCard } from '../molecules';
+import Fade from 'react-reveal/Fade';
 
 export const WorksContent = () => {
   return (
@@ -13,7 +14,9 @@ export const WorksContent = () => {
       <Center>
         <SimpleGrid spacingX="10" spacingY="5" columns={[1, 2]}>
           {i.WORK_CARDS.map((e, i) => (
-            <WorkCard key={i} {...e} />
+            <Fade key={i} bottom delay={i * 100}>
+              <WorkCard key={i} {...e} />
+            </Fade>
           ))}
         </SimpleGrid>
       </Center>
