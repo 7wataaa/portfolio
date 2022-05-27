fc49ba185246c58706124540119b5104791a9088
nashi <nn.contact.n@gmail.com>
2022-05-27 11:27:13 +0900

    [style] SkillContentにフェードイン効果を追加

--- components/organisms/SkillContent.tsx
+++ components/organisms/SkillContent.tsx
@@ -2,6 +2,7 @@ import { Box, Center, HStack, Text } from '@chakra-ui/react';
 import i from '../../texts/js.json';
 import { BgImageBox, Heading, SkillBox } from '../atoms';
 import { SkillList, SkillListWithDescription } from '../molecules';
+import Fade from 'react-reveal/Fade';
 
 export const SkillContent = () => {
   return (
@@ -12,47 +13,53 @@ export const SkillContent = () => {
         </Box>
       </Center>
 
-      <Center>
-        <SkillBox boxName="Languages" marginBottom="12">
-          <Center>
-            <HStack marginY="8" spacing="10rem" align="start">
-              <SkillList skillList={['★★', ...i.LANGUAGES.TWO_STAR]} />
-              <SkillList skillList={['★', ...i.LANGUAGES.ONE_STAR]} />
-            </HStack>
-          </Center>
-          <Center>
-            <Text fontSize="sm" whiteSpace="pre-wrap">
-              {i.LANGUAGES.STAR_DESCRIPTION}
-            </Text>
-          </Center>
-        </SkillBox>
-      </Center>
+      <Fade bottom>
+        <Center>
+          <SkillBox boxName="Languages" marginBottom="12">
+            <Center>
+              <HStack marginY="8" spacing="10rem" align="start">
+                <SkillList skillList={['★★', ...i.LANGUAGES.TWO_STAR]} />
+                <SkillList skillList={['★', ...i.LANGUAGES.ONE_STAR]} />
+              </HStack>
+            </Center>
+            <Center>
+              <Text fontSize="sm" whiteSpace="pre-wrap">
+                {i.LANGUAGES.STAR_DESCRIPTION}
+              </Text>
+            </Center>
+          </SkillBox>
+        </Center>
+      </Fade>
 
-      <Center>
-        <SkillBox boxName="Web" marginBottom="12">
-          <Center>
-            <HStack
+      <Fade bottom>
+        <Center>
+          <SkillBox boxName="Web" marginBottom="12">
+            <Center>
+              <HStack
+                marginTop="8"
+                marginBottom="5"
+                spacing="10rem"
+                align="start"
+              >
+                <SkillList spacing="10" skillList={i.WEB.COLUMN1} />
+                <SkillList spacing="10" skillList={i.WEB.COLUMN2} />
+              </HStack>
+            </Center>
+          </SkillBox>
+        </Center>
+      </Fade>
+
+      <Fade bottom>
+        <Center>
+          <SkillBox boxName="Others">
+            <SkillListWithDescription
               marginTop="8"
               marginBottom="5"
-              spacing="10rem"
-              align="start"
-            >
-              <SkillList spacing="10" skillList={i.WEB.COLUMN1} />
-              <SkillList spacing="10" skillList={i.WEB.COLUMN2} />
-            </HStack>
-          </Center>
-        </SkillBox>
-      </Center>
-
-      <Center>
-        <SkillBox boxName="Others">
-          <SkillListWithDescription
-            marginTop="8"
-            marginBottom="5"
-            skillData={i.OTHERS.data}
-          />
-        </SkillBox>
-      </Center>
+              skillData={i.OTHERS.data}
+            />
+          </SkillBox>
+        </Center>
+      </Fade>
     </BgImageBox>
   );
 };
