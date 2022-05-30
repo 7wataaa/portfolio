3e7fa6527340ed3b64148e3e87c6b7ec472f2b41
nashi <nn.contact.n@gmail.com>
2022-05-31 03:22:49 +0900

    [add] NanomemoPageに動画を追加

--- pages/works/nanomemo.tsx
+++ pages/works/nanomemo.tsx
@@ -1,5 +1,6 @@
 import { useBreakpointValue } from '@chakra-ui/react';
 import { Image } from '../../components/atoms';
+import { WorkMovie } from '../../components/molecules';
 import { WorkCarousel, WorkDescription } from '../../components/organisms';
 import { WorkPageTemplate } from '../../components/templates';
 import i from '../../texts/js.json';
@@ -25,7 +26,38 @@ const NanomemoPage = () => {
               />
             ),
           },
-          /* TODO: 動画ファイルを追加する */
+          {
+            main: (
+              <WorkMovie
+                boxSize={imageSize}
+                googleCloudMovieURL="https://drive.google.com/file/d/1Y5x02CuZWD5EW6322ilfLq39Oldn8DKB/preview"
+              />
+            ),
+            digest: (
+              <Image
+                width={imageSize}
+                height={imageSize}
+                alt="nanomemo overview-thumbnail image"
+                src="/works/nanomemo/overview-thumbnail.jpg"
+              />
+            ),
+          },
+          {
+            main: (
+              <WorkMovie
+                boxSize={imageSize}
+                googleCloudMovieURL="https://drive.google.com/file/d/168AhNGY1dx0vxvF1uLOyONpvdPkQ_NeU/preview"
+              />
+            ),
+            digest: (
+              <Image
+                width={imageSize}
+                height={imageSize}
+                alt="nanomemo email-auth-thumbnail image"
+                src="/works/nanomemo/email-auth-thumbnail.jpg"
+              />
+            ),
+          },
         ]}
       />
       <WorkDescription data={i.WORKS.NANOMEMO} />
